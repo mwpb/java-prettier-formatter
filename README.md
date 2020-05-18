8b26407d582b59c6599b18408823f61a1bfd82c8
Matthew Burke <matthewpatrickburke@gmail.com>
2020-05-18 12:22:46 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Java prettier formatter
+
+A formatter for Java files that uses the Prettier formatter.
+
+## Installation
+
+Install via the VS Code marketplace.
+
+## Usage
+
+For one time use:-
+
+* Open the Command Palette (ctrl+shift+p).
+* Select 'Format Document With...'.
+* Select 'Java prettier formatter'.
+
+To set as default formatter for Java files:-
+
+* Open the Command Palette (ctrl+shift+p).
+* Select 'Preferences: Configure Language Specific Settings'.
+* Select 'Java'.
+* Add 
+
+```
+"editor.defaultFormatter": "mwpb.java-prettier-formatter"
+```
+
+into the `[java]` section.
+
+## Acknowledgments
+
+This package completes the 'last yard' to use Prettier with Java files in VS Code.
+The hard work and credit is due to:-
+
+* [Prettier](https://prettier.io/) - Formatter.
+* [Prettier plugin Java](https://github.com/jhipster/prettier-java/tree/master/packages/prettier-plugin-java) - Java specific formatting.
---
+++ extension.js
@@ -0,0 +1,51 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const prettier = require("prettier");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  let disposable = vscode.languages.registerDocumentFormattingEditProvider(
+    "java",
+    {
+      provideDocumentFormattingEdits(document) {
+        const text = document.getText();
+        const formattedText = prettier.format(text, {
+          parser: "java",
+          tabWidth: 2,
+        });
+
+        let textEditor = vscode.window.activeTextEditor;
+        if (textEditor) {
+          let firstLine = textEditor.document.lineAt(0);
+          let lastLine = textEditor.document.lineAt(
+            textEditor.document.lineCount - 1
+          );
+          let textRange = new vscode.Range(
+            firstLine.range.start,
+            lastLine.range.end
+          );
+
+          return [vscode.TextEdit.replace(textRange, formattedText)];
+        }
+        return [];
+      },
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
